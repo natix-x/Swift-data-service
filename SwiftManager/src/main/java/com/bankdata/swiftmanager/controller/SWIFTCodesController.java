0381cbdc4922f9ac3df07dd90689ca47ed466c66
Natalia Wrześniak <natalia.wrzesniak19@gmail.com>
2025-02-17 20:05:11 +0100

    Implemented Controller Class

--- SwiftManager/src/main/java/com/bankdata/swiftmanager/controller/SWIFTCodesController.java
+++ SwiftManager/src/main/java/com/bankdata/swiftmanager/controller/SWIFTCodesController.java
@@ -1,13 +1,43 @@
 package com.bankdata.swiftmanager.controller;
 
+import com.bankdata.swiftmanager.dto.BankDTO;
+import com.bankdata.swiftmanager.dto.BranchDTO;
 import com.bankdata.swiftmanager.service.SWIFTCodesService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
+@RequestMapping("/v1/swift-codes/")
 public class SWIFTCodesController {
+
     SWIFTCodesService SWIFTCodesService;
 
     public SWIFTCodesController(com.bankdata.swiftmanager.service.SWIFTCodesService SWIFTCodesService) {
         this.SWIFTCodesService = SWIFTCodesService;
     }
+
+    @GetMapping("/{swift-code}")
+    public BankDTO getSwiftCodeDetails(@PathVariable("swift-code") String SWIFTCode)
+    {
+        return SWIFTCodesService.getSWIFTCodeDetails(SWIFTCode);
+    }
+
+    @GetMapping("/country/{countryISO2code}")
+    public List<BranchDTO> getCountryBankCodes(@PathVariable("countryISO2code") String countryISO2)
+    {
+        return SWIFTCodesService.getAllSWIFTCodesFromCountryISO2(countryISO2);
+    }
+
+    @PostMapping("/")
+    public String addBankCode(@RequestBody BankDTO bankDTO) {
+        return SWIFTCodesService.addSWIFTCode(bankDTO);
+    }
+
+    @DeleteMapping("/{swift-code}")
+    public String deleteBankCode(@PathVariable("swift-code") String SWIFTCode) {
+        return SWIFTCodesService.deleteSWIFTCode(SWIFTCode);
+    }
+
 }
